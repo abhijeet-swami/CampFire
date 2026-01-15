c37aed16e4e129a46b0f2bd781350bbd634a1ed5
Abhijeet Swami <abhijeets.swami@gmail.com>
2026-01-15 18:27:09 +0530

    chore(app): update app middleware and routing setup

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,9 +1,16 @@
 import express from "express";
+import cookieParser from "cookie-parser";
 import errorMiddleware from "./middlewares/error.middleware.js";
+
+import authRouter from "./routes/auth.route.js";
 
 const app = express();
 
 app.use(express.json({ limit: "10kb" }));
+app.use(cookieParser());
+
+app.use("/api/v1/auth", authRouter);
+
 app.use(errorMiddleware);
 
 export default app;
