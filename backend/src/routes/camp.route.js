095231ecc46de7c1c3972b6ae7ac329289987b97
Abhijeet Swami <abhijeets.swami@gmail.com>
2026-01-17 12:38:37 +0530

    feat(camps): protect camp routes and add join camp endpoint

--- backend/src/routes/camp.route.js
+++ backend/src/routes/camp.route.js
@@ -1,9 +1,11 @@
 import { Router } from "express";
 
-import { createCamp } from "../controllers/camp.controller.js";
+import { createCamp, joinCamp } from "../controllers/camp.controller.js";
+import { validateUser } from "../middlewares/validateUser.middleware.js";
 
 const router = Router();
 
-router.post("/create", createCamp);
+router.post("/create", validateUser, createCamp);
+router.post("/join/:id", validateUser, joinCamp);
 
 export default router;
