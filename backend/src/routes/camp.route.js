3eab61c12d9721f3aa5e5efbec2bf0a37be83e4e
Abhijeet Swami <abhijeets.swami@gmail.com>
2026-01-17 21:53:38 +0530

    routes: add camp join and camp feed endpoints

--- backend/src/routes/camp.route.js
+++ backend/src/routes/camp.route.js
@@ -1,11 +1,20 @@
 import { Router } from "express";
 
-import { createCamp, joinCamp } from "../controllers/camp.controller.js";
+import {
+  createCamp,
+  trendingCamps,
+  topCamps,
+  personalisedCamps,
+  joinCamp,
+} from "../controllers/camp.controller.js";
 import { validateUser } from "../middlewares/validateUser.middleware.js";
 
 const router = Router();
 
 router.post("/create", validateUser, createCamp);
 router.post("/join/:id", validateUser, joinCamp);
+router.get("/get", validateUser, personalisedCamps);
+router.get("/trending", validateUser, trendingCamps);
+router.get("/top", validateUser, topCamps);
 
 export default router;
