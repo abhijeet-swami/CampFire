37e7731fa90cd44f6baf273c657342bfeabf3107
Abhijeet Swami <abhijeets.swami@gmail.com>
2026-01-17 12:38:25 +0530

    feat(auth): add user validation middleware

---
+++ backend/src/middlewares/validateUser.middleware.js
@@ -0,0 +1,21 @@
+import { decodeToken } from "../utils/token.util.js";
+import User from "../models/user.model.js";
+import ApiError from "../utils/ApiError.util.js";
+import asyncWrapper from "../utils/asyncWrapper.util.js";
+
+const validateUser = asyncWrapper(async (req, res, next) => {
+  const token = req.cookies?.uid;
+  if (!token) throw new ApiError("Authentication required", 401);
+
+  const userId = decodeToken(token);
+  if (!userId)
+    throw new ApiError("Invalid or expired session. Please log in again.", 401);
+
+  const userExists = await User.exists({ _id: userId });
+  if (!userExists) throw new ApiError("User account not found", 404);
+
+  req.userId = userId;
+  next();
+});
+
+export { validateUser };
