79f2bc6ae71c7322e984f611336e129b12d51ff2
Abhijeet Swami <abhijeets.swami@gmail.com>
2026-01-20 14:17:38 +0530

    chore(app): register message routes

--- backend/src/app.js
+++ backend/src/app.js
@@ -5,6 +5,7 @@ import errorMiddleware from "./middlewares/error.middleware.js";
 import authRouter from "./routes/auth.route.js";
 import campRoutes from "./routes/camp.route.js";
 import postRoutes from "./routes/post.route.js";
+import messageRoutes from "./routes/message.route.js";
 
 const app = express();
 
@@ -14,6 +15,7 @@ app.use(cookieParser());
 app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/camp", campRoutes);
 app.use("/api/v1/post", postRoutes);
+app.use("/api/v1/message", messageRoutes);
 
 app.use(errorMiddleware);
 
