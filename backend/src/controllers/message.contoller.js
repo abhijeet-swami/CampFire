f3a47f224478fb5c946038b51693b76fa54d50eb
Abhijeet Swami <abhijeets.swami@gmail.com>
2026-01-20 14:17:33 +0530

    feat(messages): add get, edit, and delete message logic

--- backend/src/controllers/message.contoller.js
+++ backend/src/controllers/message.contoller.js
@@ -1,6 +1,8 @@
 import Message from "../models/message.model.js";
 import Post from "../models/post.model.js";
 import ApiError from "../utils/ApiError.util.js";
+import asyncWrapper from "../utils/asyncWrapper.util.js";
+import sendResponse from "../utils/sendResponse.util.js";
 
 const addMessage = async (userId, postId, text) => {
   const post = await Post.findById(postId).select("deleted campId").lean();
@@ -14,8 +16,79 @@ const addMessage = async (userId, postId, text) => {
     campId: post.campId,
   });
   await message.save();
+  const data = await message.populate("userId", "name username");
+
+  return data;
+};
+
+const editMessage = async (userId, messageId, text) => {
+  const message = await Message.findById(messageId).select(
+    "userId postId content",
+  );
+  if (!message) throw new ApiError("Message not found");
+  if (message.userId.toString() !== userId)
+    throw new ApiError("Only owner can edit", 400);
+
+  const post = await Post.findById(message.postId).select("deleted").lean();
+  if (post.deleted) throw new ApiError("Post is deleted", 400);
+
+  message.content = text;
+  await message.save();
 
   return message;
 };
 
-export { addMessage };
+const deleteMessage = async (userId, messageId) => {
+  const message = await Message.findById(messageId)
+    .select("userId postId")
+    .lean();
+  if (!message) throw new ApiError("Message not found");
+  if (message.userId.toString() !== userId)
+    throw new ApiError("Only owner can delete", 400);
+
+  const post = await Post.findById(message.postId).select("deleted").lean();
+  if (post.deleted) throw new ApiError("Post is deleted", 400);
+
+  await Message.findByIdAndDelete(message._id);
+
+  return true;
+};
+
+const getMessages = asyncWrapper(async (req, res) => {
+  const postId = req.params.postId;
+
+  const post = await Post.findById(postId).select("deleted").lean();
+  if (!post) throw new ApiError("Post not found", 404);
+  if (post.deleted) throw new ApiError("Post is deleted", 400);
+
+  const cursor = req.body?.cursor;
+  if (cursor === null) return sendResponse(res, 200, "No more messages");
+
+  let query = {
+    postId,
+  };
+
+  if (cursor) {
+    query.$or = [
+      { createdAt: { $lt: cursor.createdAt } },
+      { createdAt: cursor.createdAt, _id: { $lt: cursor._id } },
+    ];
+  }
+
+  const messages = await Message.find(query)
+    .populate("userId", "name username")
+    .sort({ createdAt: -1 })
+    .limit(10);
+
+  const nextCursor =
+    messages.length > 0
+      ? {
+          createdAt: messages[messages.length - 1].createdAt,
+          _id: messages[messages.length - 1]._id,
+        }
+      : null;
+
+  sendResponse(res, 200, "Message fetched!", { messages, cursor: nextCursor });
+});
+
+export { addMessage, editMessage, deleteMessage, getMessages };
