aeb7c1b57a8328809bb98a43c43714d40d3ad476
Abhijeet Swami <abhijeets.swami@gmail.com>
2026-01-20 14:17:27 +0530

    feat(messages): add HTTP route to fetch messages

---
+++ backend/src/routes/message.route.js
@@ -0,0 +1,9 @@
+import { Router } from "express";
+import { validateUser } from "../middlewares/validateUser.middleware.js";
+import { getMessages } from "../controllers/message.contoller.js";
+
+const router = Router();
+
+router.get("/get", validateUser, getMessages);
+
+export default router;
