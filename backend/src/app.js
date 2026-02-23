e9557666985c51ef07fc27fe7bf88b1df98ca19b
Abhijeet Swami <119617152+abhijeet-swami@users.noreply.github.com>
2026-02-23 08:53:08 +0530

    Add health check endpoint

--- backend/src/app.js
+++ backend/src/app.js
@@ -21,7 +21,9 @@ app.use(
 );
 app.use(express.json({ limit: "10kb" }));
 app.use(cookieParser());
-
+app.get("/health", rateLimit, (req, res) => {
+  res.json({ status: "ok" });
+});
 app.use("/api/v1/auth", rateLimit, authRouter);
 app.use("/api/v1/camp", rateLimit, campRoutes);
 app.use("/api/v1/post", rateLimit, postRoutes);
